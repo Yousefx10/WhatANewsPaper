862dc9ca37473244ac422da6aa57a15a229c7a94
Yousef10 <37476541+Yousefx10@users.noreply.github.com>
2024-07-26 19:36:54 +0300

    The Basic Structure Design

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8"/>
+        <meta charset="UTF-8"/>
         <title>READING IS FUN</title>
+
+        <link rel="stylesheet" href="style.css"/>
     </head>
     <body>
-        <h2>First Header</h2>
+        <div class="header">
+            <span>Just Displaying Some Short Information</span>
+        </div>
+
+
+        <div class="nav">
+            <hr/>
+            <h1>What A News Papers</h1>
+        </div>
     </body>
 </html>
