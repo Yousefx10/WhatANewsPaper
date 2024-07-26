41bb0f57532981dca3249e8f28cda9ad7ea15f82
Yousef10 <37476541+Yousefx10@users.noreply.github.com>
2024-07-26 20:01:45 +0300

    New Basic Options

--- index.php
+++ index.php
@@ -15,6 +15,22 @@
         <div class="nav">
             <hr/>
             <h1>What A News Papers</h1>
+
+            <div class="nav btn">
+                <span>Nav 1</span>
+                <span>Nav 2</span>
+                <span>Nav 3</span>
+                <span>Nav 4</span>
+                <span>Nav 5</span>
+            </div>
         </div>
+
+
+
+        <div class="title">
+        Enjoy Reading
+        </div>
+
+
     </body>
 </html>
